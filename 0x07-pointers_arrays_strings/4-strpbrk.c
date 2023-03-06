8253cd48c02eb7299ed725a28585c308c010539c
Ropkoy <valchebett@gmail.com>
2023-03-06 10:33:29 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,22 @@
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
-    for (i = 0; s[i] != '\0'; i++) {
-        for (j = 0; accept[j] != '\0'; j++) {
-            if (s[i] == accept[j]) {
-                return &s[i];
-            }
-        }
-    }
-    return NULL;
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[i] == accept[n])
+				return (s + i);
+		}
+	}
+
+	return (NULL);
 }
