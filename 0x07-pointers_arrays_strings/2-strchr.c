0c234c77b5ee53b5f02600d126ec2375851016ee
Ropkoy <valchebett@gmail.com>
2023-03-06 10:19:58 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,19 @@
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0' && *s != c) {
-        s++;
-    }
+	int i;
 
-    if (*s == c) {
-        return s;
-    } else {
-        return NULL;
-    }
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return (NULL);
 }
