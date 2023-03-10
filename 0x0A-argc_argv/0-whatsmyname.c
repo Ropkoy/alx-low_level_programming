2802e351bf96699dfa4c278f3817c7b3e89a542b
Ropkoy <valchebett@gmail.com>
2023-03-10 06:48:00 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    char *program_name = strdup(argv[0]);  // Make a copy of the program name
+    printf("%s\n", program_name);          // Print the program name
+    free(program_name);                    // Free the memory allocated by strdup
+    return 0;
+}
